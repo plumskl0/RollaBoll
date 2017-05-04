a5d37342ce14265bcb244cbd36bfcbf9c6d804f3
plumskl0 <plumskl0@users.noreply.github.com>
2017-05-04 18:10:13 +0200

    Changed some settings

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,16 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public SimpleTouchPad touchPad;
 
     private Rigidbody rb;
     private int count;
 
+    private Quaternion calibrationQuaternion;
+
     private void Start()
     {
+        CalibrateAccelerometer();
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
@@ -27,6 +31,12 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
+        //Vector3 rawAcceleration = Input.acceleration;
+        //Vector3 acceleration = FixAcceleration(rawAcceleration);
+        //Vector3 movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
+
+        //Vector2 direction = touchPad.GetDirection();
+        //Vector3 movement = new Vector3(direction.x, 0.0f, direction.y);
         rb.AddForce(movement * speed);
     }
 
@@ -43,9 +53,21 @@ public class PlayerController : MonoBehaviour {
     private void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        if (count >= 11)
         {
             winText.text = "You win!";
         }
+    }
+
+    void CalibrateAccelerometer ()
+    {
+        Vector3 accelerationSnapshot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerationSnapshot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
+
+    Vector3 FixAcceleration(Vector3 acceleration)
+    {
+        return calibrationQuaternion * acceleration;
     }
 }
---
+++ Assets/Scripts/SimpleTouchAreaButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SimpleTouchAreaButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool touched;
+    private int pointerID;
+    private bool canFire;
+
+    private void Awake()
+    {
+        touched = false;
+    }
+
+    public void OnPointerDown(PointerEventData data)
+    {
+        if (!touched)
+        {
+            touched = true;
+            canFire = true;
+            pointerID = data.pointerId;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.pointerId == pointerID)
+        {
+            touched = false;
+            canFire = false;
+        }
+    }
+
+    public bool CanFire()
+    {
+        return canFire;
+    }
+}
